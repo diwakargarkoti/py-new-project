3b6621bedf7977b640895a0ea91d3a3f03862cb7
diwakargarkoti <diwakargarkoti@gmail.com>
2024-05-24 09:26:10 +0530

    new

--- diwa.py
+++ diwa.py
@@ -1,2 +1,10 @@
 import streamlit as st
 st.title("Diwakar New Test")
+
+name = st.text_input("Enter Name:- ")
+mobile = st.number_input("Enter Mobile no.:- ")
+add = st.text_area("Enter your Address:- ")
+age = st.number_input("Enter your age:- ")
+Child = st.selectbox("Enter Total Child:-" ,(1,2,3,4))
+
+button = st.button("Done")
