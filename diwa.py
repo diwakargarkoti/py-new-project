38b288b4d139683aa089d0edb9ec7f077da946e2
diwakargarkoti <diwakargarkoti@gmail.com>
2024-05-24 09:37:12 +0530

    new

--- diwa.py
+++ diwa.py
@@ -4,7 +4,14 @@ st.title("Diwakar New Test")
 name = st.text_input("Enter Name:- ")
 mobile = st.number_input("Enter Mobile no.:- ")
 add = st.text_area("Enter your Address:- ")
-age = st.number_input("Enter your age:- ")
+age = st.slider("Enter your age:- ",0, 100)
 Child = st.selectbox("Enter Total Child:-" ,(1,2,3,4))
 
 button = st.button("Done")
+if button:
+    st.markdown(f"""
+    name: {name}
+    mobile: {mobile}
+    address: {add}
+    age: {age}
+    Child: {Child}""")
